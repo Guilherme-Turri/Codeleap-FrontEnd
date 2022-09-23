70c19abdec0bcf9430c684368d999bdd47ad1f93
Guilherme Turri <guilhermeturri@icloud.com>
2022-09-23 14:04:53 -0300

    add redux persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,42 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import modalDelete from './modalDelete.js';
 import modalEdit from './modalEdit.js';
 import loginUser from './loginUser.js';
 import post from './post.js';
 
-const reducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const reducers = combineReducers({
   modalDelete,
   modalEdit,
   loginUser,
   post,
 });
-const store = configureStore({ reducer });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export default store;
